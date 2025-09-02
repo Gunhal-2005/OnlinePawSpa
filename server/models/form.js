50d54443c2d2cd7e20c89089b6f645439a9aa3a0
Gunhal-2005 <144341650+Gunhal-2005@users.noreply.github.com>
2025-09-02 16:34:02 +0530

    Update form.js

--- server/models/form.js
+++ server/models/form.js
@@ -1,22 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const formSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//   },
-//   price: {
-//     type: Number,
-//     required: true,
-//   },
-// });
-
-// const Form = mongoose.model('Form', formSchema);
-// module.exports = Form;
 
 const mongoose = require("mongoose");
 
