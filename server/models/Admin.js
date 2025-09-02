af4c09c5f379601ca45b65fb60c026bac477c2ad
Gunhal-2005 <144341650+Gunhal-2005@users.noreply.github.com>
2025-09-02 16:31:43 +0530

    Update Admin.js

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,12 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const AdminSchema = new mongoose.Schema({
-//     username: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-// });
-
-// const Admin = mongoose.model("Admin", AdminSchema);
-// module.exports = Admin;
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
