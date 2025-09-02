02728ae4547ff0b88cb9db4c219faf26c29eee51
Gunhal-2005 <144341650+Gunhal-2005@users.noreply.github.com>
2025-09-02 16:32:25 +0530

    Update Appointment.js

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,38 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const appointmentSchema = new mongoose.Schema({
-//   ownerName: { type: String, required: true },
-//   petName: { type: String, required: true },
-//   service: { type: String, required: true },
-//   price: { type: String, required: true },
-//   appointmentDate: { type: String, required: true },
-//   timeSlot: { type: String, required: true },
-//   email: { type: String, required: true },
-//   contactInfo: { type: String, required: true },
-//   address: { type: String, required: true },
-//   petImage: { type: String, required: true },
-//   status: { type: String, default: 'Pending' },
-// });
-
-// module.exports = mongoose.model('Appointment', appointmentSchema);
-
-// const mongoose = require('mongoose');
-
-// const appointmentSchema = new mongoose.Schema({
-//   ownerName: String,
-//   petName: String,
-//   service: String,
-//   price: Number,
-//   appointmentDate: Date,
-//   timeSlot: String,
-//   petImage: String,
-//   email: String,
-//   contactInfo: String,
-//   address: String,
-//   status: { type: String, default: 'Pending' },
-// });
-
-// module.exports = mongoose.model('Appointment', appointmentSchema);
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
