dadaea3dbbe1205fe1208261fac3b4d6b20a7c48
Gunhal-2005 <144341650+Gunhal-2005@users.noreply.github.com>
2025-09-02 16:32:57 +0530

    Update Feedback.js

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -1,34 +1,5 @@
 
 
-// const mongoose = require("mongoose");
-
-// const feedbackSchema = new mongoose.Schema({
-//   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   content: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// const Feedback = mongoose.model("Feedback", feedbackSchema);
-// module.exports = Feedback;
-
-// const mongoose = require("mongoose");
-
-// const FeedbackSchema = new mongoose.Schema(
-//   {
-//     email: {
-//       type: String,
-//       required: true,
-//     },
-//     content: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Feedback", FeedbackSchema);
-
 const mongoose = require("mongoose");
 
 const FeedbackSchema = new mongoose.Schema(
